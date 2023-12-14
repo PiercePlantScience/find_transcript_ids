f5620ca54f78ffebfb82ae158488e548498bf0b4
Jon Xie <jon.xie@outlook.com>
2023-12-14 11:11:17 -0800

    add working regex

--- findTranscriptionIDs.sh
+++ findTranscriptionIDs.sh
@@ -7,6 +7,7 @@ INPUTDIR=./input
 OUTPUTDIR=./output
 LOGDIR=./log
 RUNLOG=$LOGDIR/RUNLOG
+NOTFOUNDLOG=$LOGDIR/NOTFOUND
 FIND_RESULTS_FILE=$OUTPUTDIR/findTranscriptionIDs.tsv.txt
 [ ! -d "$INPUTDIR" ] && mkdir $INPUTDIR
 [ ! -d "$OUTPUTDIR" ] && mkdir $OUTPUTDIR
@@ -24,35 +25,26 @@ echo
 #
 
 # extract index transcriptionIDs from quant.sf file to drive our main loop
-TRANSCRIPTION_IDS_20=$(grep TRI $QUANT_FILE| awk '{print $1}'| sort -u| head -20)     # TEST MODE: run only 1st 20
-#TRANSCRIPTION_IDS=$(grep TRI $QUANT_FILE| awk '{print $1}'| sort -u)
+##TRANSCRIPTION_IDS_20=$(grep TRI $QUANT_FILE| awk '{print $1}'| sort -u| head -200)     # TEST MODE: run only 1st 20
+TRANSCRIPTION_IDS=$(grep TRI $QUANT_FILE| awk '{print $1}'| sort -u|tail -73)
 
 echo "Results will be written to: (${FIND_RESULTS_FILE})"
-echo "Seeking $(echo "$TRANSCRIPTION_IDS_20"| wc -l) unique TransctionIDs from file: ${QUANT_FILE##*/}"
+echo "Seeking $(echo "$TRANSCRIPTION_IDS"| wc -l) unique TransctionIDs from file: ${QUANT_FILE##*/}"
 
 
 # MAIN LOOP
-for TID in $(echo "$TRANSCRIPTION_IDS_20")
+for TID in $(echo "$TRANSCRIPTION_IDS")
 do
     QUANT_BITS=$(grep $TID $QUANT_FILE|tr -d '\n')
     TRINOTATE_BITS=$(grep $TID $TRINOTATE_FILE|cut -f3,7|tr -d '\n')
 
     echo -n "."
-    # log for Not found in Trinotate
-    ##[ -z "$TRINOTATE_BITS" ] && echo "$TID: NOT FOUND" >> $RUNLOG  
-    #[[ "$TRINOTATE_BITS" =~ "(?=.*\t)(?=.*\.)" ]] && echo "$TID: NOT FOUND" >> $RUNLOG || echo "$TID: $TRINOkTATE_BITS" >> $RUNLOG
-    #regex_trinotate_empty="^[\t.]*$"
-    ##regex_trinotate_empty='^[\.\t]+$'
-    #regex_trinotate_empty='(^|\t)(\t|\.|..)($|\t)'
-    regex_trinotate_empty='^(\.?|\.\.?)\t(\.?|\.\.?)\t(\.?|\.\.?)\t(\.?|\.\.?)$'
-    ##[[ "$TRINOTATE_BITS" =~ "^[.\t]+$" ]] && echo "$TID: NOT FOUND" >> $RUNLOG || echo "$TID: $TRINOTATE_BITS" >> $RUNLOG
-    #[[ "$TRINOTATE_BITS" =~ ^[\.\t]+$ ]] && echo "$TID: NOT FOUND" >> $RUNLOG
-    [[ $TRINOTATE_BITS =~ $regex_trinotate_empty ]] && echo "$TID: NOT FOUND" >> $RUNLOG
-    #echo "$TID: $TRINOTATE_BITS" >> ./trinotate_bits
+    regex_trinotate_empty="^.+[A-Z]+.+$"        # inverse match, good eough?
+    #[[ $TRINOTATE_BITS =~ $regex_trinotate_empty ]] || echo "$TID: NOT FOUND" >> $RUNLOG
+    [[ $TRINOTATE_BITS =~ $regex_trinotate_empty ]] || echo -e "$QUANT_BITS\t$TRINOTATE_BITS" >> $NOTFOUNDLOG
     
     # out put results
-    ##[ ! -z "$TRINOTATE_BITS" ] && echo -e "$QUANT_BITS\t$TRINOTATE_BITS" >> $FIND_RESULTS_FILE 
-    [[ "$TRINOTATE_BITS" =~ "^\.\t\.$" ]] || echo -e "$QUANT_BITS\t$TRINOTATE_BITS" >> $FIND_RESULTS_FILE
+    [[ $TRINOTATE_BITS =~ $regex_trinotate_empty ]] && echo -e "$QUANT_BITS\t$TRINOTATE_BITS" >> $FIND_RESULTS_FILE
 done
 
 echo Done.
