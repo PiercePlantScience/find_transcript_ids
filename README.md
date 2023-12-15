a6323f900abfea56a957d8d69900dad85ea6cade
Jon Xie <jon.xie@outlook.com>
2023-12-14 17:12:32 -0800

    add aggregated header to output files

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## findTranscriptionIDs
 
 findTranscriptionIDs.sh - refactor script for correct WBC quant.sf files
+
+- script finished/tested: see attached zip, pending UAT.
+- script now take input parameters of (quant.sf, trinotate_report) files & will default to looking for the named files in input/wbc_quant directory.
+- script now outputs matches with empty BLASTX & BLASTP hits to log/NOTFOUND
+- added aggregated header for output files
+- TODO: add similarity% elimination logic by comparing BLAST data, if required; 2) make script both callable user & scripts
+
+(updated 12/14/23)
--- findTranscriptionIDs.sh
+++ findTranscriptionIDs.sh
@@ -8,12 +8,18 @@ OUTPUTDIR=./output
 LOGDIR=./log
 RUNLOG=$LOGDIR/RUNLOG
 NOTFOUNDLOG=$LOGDIR/NOTFOUND
+
+# aggregated output header
+AGGREATED_HEADER='Name\tLength\tEffectiveLength\tTPM\tNumReads\tsprot_Top_BLASTX_hit\tsprot_Top_BLASTP_hit'
 FIND_RESULTS_FILE=$OUTPUTDIR/findTranscriptionIDs.tsv.txt
+
+# make & reset stuff..
 [ ! -d "$INPUTDIR" ] && mkdir $INPUTDIR
 [ ! -d "$OUTPUTDIR" ] && mkdir $OUTPUTDIR
 [ ! -d "$LOGDIR" ] && mkdir $LOGDIR
 [ -f "$RUNLOG" ] && > $RUNLOG
-[ -f "$FIND_RESULTS_FILE" ] && > $FIND_RESULTS_FILE
+[ -f "$NOTFOUNDLOG" ] && echo -e "$AGGREATED_HEADER"> $NOTFOUNDLOG
+[ -f "$FIND_RESULTS_FILE" ] && echo -e "$AGGREATED_HEADER"> $FIND_RESULTS_FILE
 
 # input files
 QUANT_FILE=${1:-$INPUTDIR/wbc_quant/wbc.quant.sf}
@@ -22,10 +28,9 @@ echo "Matching up TranscriptIDs for following files:"
 echo "(${QUANT_FILE##*/})"
 echo "(${TRINOTATE_FILE##*/})"
 echo
-#
 
 # extract index transcriptionIDs from quant.sf file to drive our main loop
-##TRANSCRIPTION_IDS_20=$(grep TRI $QUANT_FILE| awk '{print $1}'| sort -u| head -200)     # TEST MODE: run only 1st 20
+##TRANSCRIPTION_IDS_20=$(grep TRI $QUANT_FILE| awk '{print $1}'| sort -u| head -200)     # TEST EX.: to run only 1st 200
 TRANSCRIPTION_IDS=$(grep TRI $QUANT_FILE| awk '{print $1}'| sort -u)
 
 echo "Results will be written to: (${FIND_RESULTS_FILE})"
@@ -39,7 +44,7 @@ do
     TRINOTATE_BITS=$(grep $TID $TRINOTATE_FILE|cut -f3,7|tr -d '\n')
 
     echo -n "."
-    regex_trinotate_empty="^.+[A-Z]+.+$"        # inverse match, good eough?
+    regex_trinotate_empty="^.+[A-Z]+.+$"        # inverse match, is this good enough?
     #[[ $TRINOTATE_BITS =~ $regex_trinotate_empty ]] || echo "$TID: NOT FOUND" >> $RUNLOG
     [[ $TRINOTATE_BITS =~ $regex_trinotate_empty ]] || echo -e "$QUANT_BITS\t$TRINOTATE_BITS" >> $NOTFOUNDLOG
     
