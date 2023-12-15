904a7a6f80bcad9537263f51f44525efa9f7cd60
Jon Xie <jon.xie@outlook.com>
2023-12-15 09:35:48 -0800

    commit with TPM & Length filters for next batch run

--- findTranscriptionIDs.sh
+++ findTranscriptionIDs.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 #
 #
+# QUANT FILTERS
+MINQ_TPM=10
+MINQ_LENGTH=1000
 
 #
 INPUTDIR=./input
@@ -24,18 +27,19 @@ EMPTY_RESULTS_FILE=$OUTPUTDIR/emptyTranscriptionIDs.tsv.txt
 
 startTimeInSec=$(date +%s)
 STARTTIME=$(date -d "@$startTimeInSec" +"%D %T")
-[ -f "$RUNLOG" ] && echo "(${0##*/}) started at: $STARTTIME">$RUNLOG
+[ -f "$RUNLOG" ] && echo "[${0##*/}] started at: $STARTTIME">$RUNLOG
 
 # input files
 QUANT_FILE=${1:-$INPUTDIR/wbc_quant/wbc.quant.sf}
-TRINOTATE_FILE=${2:-$INPUTDIR/wbc_quant/trinotate_annotation_report.xls}
+ANNOTATION_FILE=${2:-$INPUTDIR/wbc_quant/trinotate_annotation_report.xls}
 echo "Matching up TranscriptIDs for following files:"
 echo "(${QUANT_FILE##*/})"
-echo "(${TRINOTATE_FILE##*/})"
+echo "(${ANNOTATION_FILE##*/})"
 echo
 
 # extract index transcriptionIDs from quant.sf file to drive our main loop
 ##TRANSCRIPTION_IDS_20=$(grep TRI $QUANT_FILE| awk '{print $1}'| sort -u| head -200)     # TEST EX.: to run only 1st 200
+#TRANSCRIPTION_IDS=$(grep TRI "$QUANT_FILE"| awk '{print $1}'| sort -u| tail -33)
 TRANSCRIPTION_IDS=$(grep TRI "$QUANT_FILE"| awk '{print $1}'| sort -u)
 
 echo "Results will be written to: (${FOUND_RESULTS_FILE})"
@@ -45,41 +49,53 @@ echo "Seeking $(echo "$TRANSCRIPTION_IDS"| wc -l) unique TransctionIDs from file
 # MAIN LOOP
 for TID in $TRANSCRIPTION_IDS
 do
-    QUANT_BITS=$(grep "$TID" "$QUANT_FILE"| tr -d '\n')
-    #ANNOTATION_BITS=$(grep "$TID" "$TRINOTATE_FILE"| cut -f3,7| tr -d '\n')
-    ANNOTATION_BITS=$(grep "$TID" "$TRINOTATE_FILE"| cut -f3,7| uniq)
+    QUANT_BITS=$(grep "$TID" "$QUANT_FILE"| uniq| tr -d '\n')
+    quant_length=$(echo "$QUANT_BITS"| cut -f2)
+    quant_length=${quant_length%.*}
+    quant_tpm=$(echo "$QUANT_BITS"| cut -f4)
+    quant_tpm=${quant_tpm%.*}
+
+    # QUANT FILTER SECTION
+    if [ "$quant_tpm" -lt "$MINQ_TPM" ] && [ "$quant_length" -lt "$MINQ_LENGTH" ]
+    then
+        echo "$TID: TPM($quant_tpm), Length($quant_length) QuantFILTERED">>$RUNLOG
+        continue
+    fi
+
+    ANNOTATION_BITS=$(grep "$TID" "$ANNOTATION_FILE"| cut -f3,7| uniq)
     [ -z "$ANNOTATION_BITS" ] && numMatch=0 || numMatch=$(echo "$ANNOTATION_BITS"| wc -l)
 
     echo -n "."
-    regex_trinotate_empty="^.+[A-Z]+.+$"        # inverse match, is this good enough?
-    #[[ $ANNOTATION_BITS =~ $regex_trinotate_empty ]] || echo "$TID: NOT FOUND" >> $RUNLOG
-    #[[ $ANNOTATION_BITS =~ $regex_trinotate_empty ]] || echo -e "$QUANT_BITS\t$ANNOTATION_BITS" >> $NOTFOUNDLOG
-    [[ $ANNOTATION_BITS =~ $regex_trinotate_empty ]]|| echo -e "$QUANT_BITS\t$(echo "$ANNOTATION_BITS"|tr -d '\n')">>$EMPTY_RESULTS_FILE
+
+    # REGEX ANNOTATION EMPTY
+    regex_annotation_empty="^.+[A-Z]+.+$"        # inverse match, is this good enough?
+
+    [[ $ANNOTATION_BITS =~ $regex_annotation_empty ]]|| echo -e "$(echo "$QUANT_BITS"|tr -d '\n')\t$(echo "$ANNOTATION_BITS"|tr -d '\n')">>$EMPTY_RESULTS_FILE
     
     if [ "$numMatch" -gt 0 ]
     then
         # for MATCHED_BITS in ${ANNOTATION_BITS}
         # do
         #     # output results
-        #     #[[ $ANNOTATION_BITS =~ $regex_trinotate_empty ]]&& echo -e "$QUANT_BITS\t$ANNOTATION_BITS">>$FOUND_RESULTS_FILE
+        #     #[[ $ANNOTATION_BITS =~ $regex_annotation_empty ]]&& echo -e "$QUANT_BITS\t$ANNOTATION_BITS">>$FOUND_RESULTS_FILE
         #     #MATCHED_BITS=${MATCHED_BITS/s/[[:space:]]*$//}
         #     MATCHED_BITS=$(echo "$MATCHED_BITS"|tr -d '\n')
-        #     [[ $MATCHED_BITS =~ $regex_trinotate_empty ]]&& echo -e "$QUANT_BITS\t$MATCHED_BITS">>$FOUND_RESULTS_FILE
+        #     [[ $MATCHED_BITS =~ $regex_annotation_empty ]]&& echo -e "$QUANT_BITS\t$MATCHED_BITS">>$FOUND_RESULTS_FILE
         # done
         while IFS= read -r MATCHED_BITS; do
             # Process each line here
              MATCHED_BITS=$(echo "$MATCHED_BITS"|tr -d '\n')
-             [[ $MATCHED_BITS =~ $regex_trinotate_empty ]]&& echo -e "$QUANT_BITS\t$MATCHED_BITS">>$FOUND_RESULTS_FILE
+             [[ $MATCHED_BITS =~ $regex_annotation_empty ]]&& echo -e "$QUANT_BITS\t$MATCHED_BITS">>$FOUND_RESULTS_FILE
         done <<< "$ANNOTATION_BITS"
         echo "$TID: found($numMatch)">>$RUNLOG
     else
         # log notfound
-        echo "$TID: NOTFOUND">>$RUNLOG
+        echo "$TID: TPM($quant_tpm), Length($quant_length): NOTFOUND">>$RUNLOG
     fi
 done
 
 echo Done.
 stopTimeInSec=$(date +%s)
 STOPTIME=$(date -d "@$stopTimeInSec" +"%D %T")
-echo "(${0##*/}) ended at: $STOPTIME">>$RUNLOG
+echo "[${0##*/}] ended at: $STOPTIME">>$RUNLOG
 echo "Total runtime was: $((stopTimeInSec - startTimeInSec)) seconds."
